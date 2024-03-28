a54d82a9d1e97f0089c63bf070fffd114953091d
Giovani Murakami <giovani.murakami@outlook.com>
2024-03-28 13:36:25 -0300

    Added emojis

--- controllers/SorterController.js
+++ controllers/SorterController.js
@@ -1,29 +1,6 @@
 const axios = require("axios");
 const capitalize = require("../helpers/capitalize");
 
-/* module.exports = class SorterController {
-    static sort(req, res) {
-        res.render("sort/home");
-    }
-    static async sorter(req, res) {
-        fetch("http://192.168.17.113/sorteio-api/sorteio/api/v1/winner.php")
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const winner = data;
-                console.log(winner);
-                if (winner.Status) {
-                    res.render("sort/winner", { winner });
-                }
-                SorterController.sorter;
-            })
-            .catch((error) => {
-                console.error("Houve um problema com a solicitação:", error);
-            });
-    }
-}; */
-
 module.exports = class SorterController {
     static sort(req, res) {
         res.render("sort/home");
@@ -35,8 +12,9 @@ module.exports = class SorterController {
             );
             const winner = response.data;
             winner.Name = capitalize(winner.Name);
+            console.log(winner);
             if (!winner.Status) {
-                SorterController.sorter;
+                SorterController.sorter(req, res);
                 return;
             }
             res.render("sort/winner", { winner });
